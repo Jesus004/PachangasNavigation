5da20f57fe067688395f7b962302f26fad2af756
Jesus004 <nuniezsanmiguel@hotmail.com>
2021-09-08 23:19:09 +0200

    fragmentos

--- app/src/main/java/com/txus/pachangasnavigation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/txus/pachangasnavigation/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.txus.pachangasnavigation
+package com.txus.pachangasnavigation.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.txus.pachangasnavigation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
